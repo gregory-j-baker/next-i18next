565b82c64c598516b9f19a0944c687c777f0925d
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2020-10-13 15:27:40 -0230

    Switch to _only_ localStorage detection

--- examples/simple/i18n.js
+++ examples/simple/i18n.js
@@ -1,9 +1,13 @@
-const NextI18Next = require('next-i18next').default
-const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
-const path = require('path')
+const NextI18Next = require("next-i18next").default;
+const { localeSubpaths } = require("next/config").default().publicRuntimeConfig;
+const path = require("path");
 
 module.exports = new NextI18Next({
-  otherLanguages: ['de'],
+  otherLanguages: ["de"],
   localeSubpaths,
-  localePath: path.resolve('./public/static/locales')
-})
+  localePath: path.resolve("./public/static/locales"),
+  detection: {
+    caches: ["localStorage"],
+    order: ["localStorage"],
+  },
+});
